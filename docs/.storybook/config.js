2946024a6d15655e70dba76228a9ef8e6293b76b
Aurore <aurorem@theodo.fr>
2017-11-18 19:08:11 +0100

    Remove useless ./.storybook folder

--- docs/.storybook/config.js
+++ docs/.storybook/config.js
@@ -1,14 +1,4 @@
-import { setOptions } from '@storybook/addon-options';
 import { configure } from '@storybook/react';
-
-setOptions({
-  name: 'Maps',
-  url: 'https://react-native-web-community.github.io/react-native-web-maps',
-  goFullScreen: false,
-  showLeftPanel: true,
-  showDownPanel: false,
-  downPanelInRight: false,
-});
 
 function loadStories() {
   require('../stories');
