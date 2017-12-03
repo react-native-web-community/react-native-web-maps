a728908fae5d0da799df752d57fd04846384403d
Aurore <aurorem@theodo.fr>
2017-12-03 12:49:48 +0100

    Use storybook actions to display coordinates onChange

--- docs/.storybook/config.js
+++ docs/.storybook/config.js
@@ -1,4 +1,13 @@
 import { configure } from '@storybook/react';
+import { setOptions } from '@storybook/addon-options';
+setOptions({
+  name: 'Maps',
+  url: 'https://react-native-web-maps.github.io/react-native-web-maps',
+  goFullScreen: false,
+  showLeftPanel: true,
+  showDownPanel: true,
+  downPanelInRight: false,
+});
 
 function loadStories() {
   require('../stories');
--- docs/stories/index.js
+++ docs/stories/index.js
@@ -3,19 +3,15 @@ import { Text, View } from 'react-native';
 import MapView from 'react-native-maps';
 
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 
 storiesOf('MapView', module)
   .add('Simple display', () => <MapView region={{ latitude: 48.86, longitude: 2.34 }} />)
-  .add('Display latitude and longitude on region change', () => {
-    const onRegionChangeComplete = ({ latitude, longitude }) => {
-      alert(`latitude : ${latitude}, longitude: ${longitude}`);
-    };
-    return (
-      <View>
-        <MapView region={{ latitude: 48.86, longitude: 2.34 }} onRegionChangeComplete={onRegionChangeComplete} />
-      </View>
-    );
-  });
+  .add('Display latitude and longitude on region change', () => (
+    <View>
+      <MapView region={{ latitude: 48.86, longitude: 2.34 }} onRegionChangeComplete={action('new coordinates')} />
+    </View>
+  ));&
 
 storiesOf('Marker', module).add('Simple display', () => (
   <MapView region={{ latitude: 48.88, longitude: 2.32 }}>
