2b2288e7073d367cc6d9b917f974e9f8b7b2061a
Aurore <aurorem@theodo.fr>
2017-12-03 20:59:12 +0100

    Rename stories

--- docs/stories/index.js
+++ docs/stories/index.js
@@ -6,14 +6,14 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
 storiesOf('MapView', module)
-  .add('Simple display', () => <MapView region={{ latitude: 48.86, longitude: 2.34 }} />)
-  .add('Display latitude and longitude on region change', () => (
+  .add('basic', () => <MapView region={{ latitude: 48.86, longitude: 2.34 }} />)
+  .add('onRegionChangeComplete', () => (
     <View>
       <MapView region={{ latitude: 48.86, longitude: 2.34 }} onRegionChangeComplete={action('new coordinates')} />
     </View>
   ));&
 
-storiesOf('Marker', module).add('Simple display', () => (
+storiesOf('Marker', module).add('basic', () => (
   <MapView region={{ latitude: 48.88, longitude: 2.32 }}>
     <MapView.Marker title="BAM" coordinate={{ latitude: 48.8828463, longitude: 2.3229091 }} />
   </MapView>
