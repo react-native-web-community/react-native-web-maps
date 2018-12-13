5849903bd7bb7648e68a30164e5a09fa4f083deb
adnan1naeem <adnan.naeem@blueeast.com>
2018-12-13 16:06:15 +0500

    fix:fixed map

--- src/Marker.js
+++ src/Marker.js
@@ -5,6 +5,8 @@ class MapViewMarker extends Component {
   render() {
     return (
       <Marker
+        draggable={this.props.draggable}
+        onDragEnd={this.props.onDragEnd}
         title={this.props.description ? `${this.props.title}\n${this.props.description}` : this.props.title}
         position={{ lat: this.props.coordinate.latitude, lng: this.props.coordinate.longitude }}
       />
