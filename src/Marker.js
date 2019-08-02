eff57ff2487c45848a2d71ced8a70d49b40d65da
Eric Butler <eric@level.com>
2019-08-02 17:24:13 -0400

    Added support for custom markers

--- src/Marker.js
+++ src/Marker.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
-import { Marker } from 'react-google-maps';
+import { Marker, OverlayView } from 'react-google-maps';
+
+const ConditionalWrap = ({ condition, wrap, children }) =>
+  condition ? wrap(children) : <React.Fragment>{children}</React.Fragment>;
 
 class MapViewMarker extends Component {
   render() {
-    const { description, title, coordinate, ...rest } = this.props;
+    const { description, title, coordinate, children, ...rest } = this.props;
+    if (children) {
+      const { onPress, ...overlayRest } = rest;
+      return (
+        <OverlayView
+          {...overlayRest}
+          title={description ? `${title}\n${description}` : title}
+          position={{ lat: coordinate.latitude, lng: coordinate.longitude }}
+          mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+        >
+          <ConditionalWrap
+            condition={!!onPress}
+            wrap={(c) => (
+              <div ref={(ref) => ref && google.maps.OverlayView.preventMapHitsFrom(ref)} onClick={onPress}>
+                {c}
+              </div>
+            )}
+          >
+            {children}
+          </ConditionalWrap>
+        </OverlayView>
+      );
+    }
     return (
       <Marker
         {...rest}
