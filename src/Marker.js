fa20ec40ea20ebe7bb081b3c3efa000c3b011f95
Eric Butler <eric@Level-Loaner-1.local>
2019-08-05 15:25:20 -0400

    cleanup

--- src/Marker.js
+++ src/Marker.js
@@ -1,31 +1,25 @@
 import React, { Component } from 'react';
 import { Marker, OverlayView } from 'react-google-maps';
 
-const ConditionalWrap = ({ condition, wrap, children }) =>
-  condition ? wrap(children) : <React.Fragment>{children}</React.Fragment>;
-
 class MapViewMarker extends Component {
   render() {
     const { description, title, coordinate, children, ...rest } = this.props;
     if (children) {
       const { onPress, ...overlayRest } = rest;
+      const wrappedChildren = onPress ? (
+        <div ref={ref => ref && google.maps.OverlayView.preventMapHitsFrom(ref)} onClick={onPress}>
+          {children}
+        </div>
+      ) : (
+        children
+      );
       return (
         <OverlayView
           {...overlayRest}
           title={description ? `${title}\n${description}` : title}
           position={{ lat: coordinate.latitude, lng: coordinate.longitude }}
-          mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
-        >
-          <ConditionalWrap
-            condition={!!onPress}
-            wrap={(c) => (
-              <div ref={(ref) => ref && google.maps.OverlayView.preventMapHitsFrom(ref)} onClick={onPress}>
-                {c}
-              </div>
-            )}
-          >
-            {children}
-          </ConditionalWrap>
+          mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}>
+          {wrappedChildren}
         </OverlayView>
       );
     }
